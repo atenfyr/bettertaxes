1fb65c324315f0ef4ef3ef5312d550d2c3975efb
Alex <atenefyr@gmail.com>
2019-01-05 14:51:01 -0600

    fix up indents

---
+++ BetterTaxes.cs
@@ -0,0 +1,14 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterTaxes {
+    class BetterTaxes : Mod {
+        public BetterTaxes() {
+            Properties = new ModProperties() {
+                Autoload = true
+            };
+        }
+    }
+}
